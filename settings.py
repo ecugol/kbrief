bbd588054557f682923c627ba517081a4676f8e1
Ivan Vedernikov <ivan@klice.ru>
2012-01-06 20:53:20 +0600

    Latest changes

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = '/user_media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = rel('site_media')
+STATIC_ROOT = rel('static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,7 +72,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    rel('static'),
+rel('static'),    
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
